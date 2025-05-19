19581e350147accaa053ff5837d17fd49fc096df
BINIBEAL1 <bnigussie13@gmail.com>
2025-05-19 14:58:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 def start(update, context):
     update.message.reply_text('Hello! I am your bot. 🚀')
 
-TOKEN = os.environ["TELEGRAM_TOKEN"]
+TOKEN = os.environ["8117787100:AAHP8uShPCYnwijakNXBqdAAVKHqfYL-zq8"]
 updater = Updater(TOKEN, use_context=True)
 updater.dispatcher.add_handler(CommandHandler("start", start))
 updater.start_polling()
